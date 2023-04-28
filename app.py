4d54bce0d8d4aca4ca764a3f7bc13576ff997c43
Albanus Muange Mutunga <albanusmuangemutunga@gmail.com>
2023-04-28 08:32:42 +0300

    Updated files

---
+++ app.py
@@ -0,0 +1,4 @@
+def greet_user(name):
+    print(name)
+
+greet_user('John')
---
+++ main.py
@@ -0,0 +1,39 @@
+
+#selection sort
+
+
+def sort(nums):
+    for i in range(5):
+        minpos = i
+        for j in range(i, 6):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+        print(nums)
+
+
+nums = [5, 3, 8, 6, 7, 2]
+sort(nums)
+
+print(nums)
+
+#bubble sort
+
+
+def sort(nums):
+    for i in range(len(nums)-1, 0, -1):
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+
+
+nums = [5, 3, 8, 6, 7, 2]
+
+sort(nums)
+print('This is bubble sort solution')
+print(nums)
